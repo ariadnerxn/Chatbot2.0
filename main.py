13f61be9a8f552c3304e4ad1b4db54fa9faef0fe
Elenna Wren <elennawren@gmail.com>
2024-09-30 23:24:39 +0000

    Redo chatbox code

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello CodeSandbox!")
+print("Hello, welcome to the ChatBot.")
+name = input('What is your name? ')
+age = input('What about your age? ')
+help = input('How may I help you? ')
+
+print('Choose from this list: \n')
+print('1. \n') #Put something here
+print('2. \n') #Put something here
+print('3. \n') #Put something here
+choiceTwo = 'y'
+while choiceTwo == 'y':
+    choice = input('\nType number answer here: ')
+    if choice == '1':
+        print('')#Place things here
+    elif choice == '2':
+        print('')#Place things here
+    elif choice == '3':
+        print('')#Place things here
+    else:
+        choice = input("Enter your choice again: ")
+    choiceTwo = input('Would you like to go again(y/n)? ')
+print(' ')
